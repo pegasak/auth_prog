61ec8aab7b676992def9c12dbe730b3562c202be
pegasak <Pegas05bng@mail.ru>
2024-10-07 17:01:48 +0300

    created bot

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
+def checking():
+    login = os.getenv("APP_LOGIN")
+    password = os.getenv('APP_PASSWORD')
 
-login = os.getenv("APP_LOGIN")
-password = os.getenv('APP_PASSWORD')
+    if login and password:
+        anonymus_login = input('Type your login: ')
+        anonymus_password = input('Type your password: ')
 
-if login and password:
-    anonymus_login = input('Type your login: ')
-    anonymus_password = input('Type your password: ')
-
-    if login == anonymus_login and password == anonymus_password:
-        print('You are successfully logged in')
+        if login == anonymus_login and password == anonymus_password:
+            print('You are successfully logged in')
+        else:
+            print('Login or password is incorrect')
     else:
-        print('Login or password is incorrect')
-else:
-    print('Environment variables not set')
+        print('Environment variables not set')
+
+if __name__ == "__main__":
+    checking()
+
---
+++ tg_bot_2.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+load_dotenv()
+
+bot = telebot.TeleBot('7764563323:AAFIWwntdhVt0GI3QtRC0gcZLLwieBvTYws')
+
+user_login = {}
+user_password = {}
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id,
+                     f'Здравствуйте, {message.from_user.first_name} {message.from_user.last_name}, я бот, который выполнит вашу авторизацию.'
+                     '\nФункции моей работы:'
+                     '\n1) /authorization - выполняет вашу авторизацию.')
+
+
+@bot.message_handler(commands=['authorization'])
+def start_authorization(message):
+    bot.send_message(message.chat.id, "Введите ваш логин:")
+    bot.register_next_step_handler(message, process_login_step)
+
+
+def process_login_step(message):
+    user_login[message.chat.id] = message.text
+    bot.send_message(message.chat.id, "Введите ваш пароль:")
+    bot.register_next_step_handler(message, process_password_step)
+
+
+def process_password_step(message):
+    user_password[message.chat.id] = message.text
+    login = os.getenv("APP_LOGIN")
+    password = os.getenv("APP_PASSWORD")
+
+    if login == user_login[message.chat.id] and password == user_password[message.chat.id]:
+        bot.send_message(message.chat.id, "Вы успешно авторизовались!")
+    else:
+        bot.send_message(message.chat.id, "Неверный логин или пароль.")
+
+
+bot.polling(none_stop=True)
